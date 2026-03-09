Fixed flatten, scaling issue

Lab_1/code/q2_gradient.py
@@ -101,47 +101,54 @@ def compute_full_gradient(words_x, words_y, W, T):
     avg_grad_w = grad_w_sum / n
     avg_grad_t = grad_t_sum / n
     return avg_loop, avg_grad_w, avg_grad_t
+    
 def objective_and_grad(params, words_x, words_y, d=128, K=26, C=1000):
     W = params[:K*d].reshape(K, d)
     T = params[K*d:].reshape(K, K)
+    
     total_log = 0.0
     grad_w_sum = np.zeros_like(W)
     grad_t_sum = np.zeros_like(T)
+    
     for X, y in zip(words_x, words_y):
         log_p, grad_w, grad_t = compute_gradient(X, y, W, T)
         total_log += log_p
         grad_w_sum += grad_w
         grad_t_sum += grad_t
+        
     n = len(words_x)
+    avg_log = total_log / n
     avg_grad_w = grad_w_sum / n
     avg_grad_t = grad_t_sum / n
-    obj = -C * total_log + 0.5 * np.sum(W**2) + 0.5 * np.sum(T**2)
+    
+    obj = -C * avg_log + 0.5 * np.sum(W**2) + 0.5 * np.sum(T**2)
     grad_w = -C * avg_grad_w + W
     grad_t = -C * avg_grad_t + T
-    grad = np.concatenate([grad_w.flatten(), grad_t.flatten('F')])
+    grad = np.concatenate([grad_w.ravel(), grad_t.ravel()])
     return obj, grad
     
 def save_solution(filename, grad_w, grad_t):
-    vec = np.concatenate([grad_w.flatten(), grad_t.flatten('F')])
+    vec = np.concatenate([grad_w.ravel(), grad_t.ravel()])
     np.savetxt(filename, vec)
 
+'''
 # #2a
-# W, T = load_model("/Users/tylerstrach/Desktop/10 - Spring 2026/lab1/CS_512_Lab_1/Lab_1/data/model.txt")
-# words_x, words_y = load_train("/Users/tylerstrach/Desktop/10 - Spring 2026/lab1/CS_512_Lab_1/Lab_1/data/train.txt")
-# avg_log, avg_grad_w, avg_grad_t = compute_full_gradient(words_x, words_y, W, T) 
-# save_solution("../result/gradient.txt", avg_grad_w, avg_grad_t)
-# print("Average log p(y|X) over training set:", avg_log) # -4.140274439213334
+W, T = load_model("../data/model.txt")
+words_x, words_y = load_train("../data/train.txt")
+avg_log, avg_grad_w, avg_grad_t = compute_full_gradient(words_x, words_y, W, T) 
+save_solution("../result/gradient.txt", avg_grad_w, avg_grad_t)
+print("Average log p(y|X) over training set:", avg_log) # -4.140274439213334
 
 # #2b
-# W_init = np.zeros((26, 128))
-# T_init = np.zeros((26, 26))
-# params_init = np.concatenate([W_init.flatten(), T_init.flatten('F')])
-# solution = fmin_tnc(func=lambda p, *args: objective_and_grad(p, *args), x0=params_init, args=(words_x, words_y), messages=0)
-# params_opt = solution[0]
-# W_opt = params_opt[:26*128].reshape(26, 128)
-# T_opt = params_opt[26*128:].reshape(26, 26, order='F')
-# save_solution("../result/solution.txt", W_opt, T_opt)
-
+W_init = np.zeros((26, 128))
+T_init = np.zeros((26, 26))
+params_init = np.concatenate([W_init.ravel(), T_init.ravel()])
+solution = fmin_tnc(func=lambda p, *args: objective_and_grad(p, *args), x0=params_init, args=(words_x, words_y), messages=0)
+params_opt = solution[0]
+W_opt = params_opt[:26*128].reshape(26, 128)
+T_opt = params_opt[26*128:].reshape(26, 26, order='F')
+save_solution("../result/solution.txt", W_opt, T_opt)
+'''
 
  
   